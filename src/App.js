48927ffaa70882fdd6ef654c9100141605d8cda7
BartoszJS <bartosz290300@wp.pl>
2022-12-13 02:04:39 +0100

    62% notebook

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ const App = () => {
   return (
     <div className='app'>
       <Navbar />
-      {/* <NavigationDots />
-      <SocialMedia /> */}
+      <NavigationDots />
+      <SocialMedia />
       <Header />
       <About />
       <Work />
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -18,14 +18,14 @@ const scaleVariants = {
 const Header = () => {
   return (
     <div className='app__header'>
+      <div className='text'>
+        {/* <div className='border-text'>
+          <div className='dot'></div>
+          BARTOSZ
+          <div className='dot'>{}</div>
+        </div> */}
+      </div>
       <div className='photo'>
-        <div className='text'>
-          <div className='border-text'>
-            <div className='dot'></div>
-            BARTOSZ
-            <div className='dot'>{}</div>
-          </div>
-        </div>
         {/* <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
